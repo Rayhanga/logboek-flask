ebad91c7c2e0a3d4e9e28a7d9fcd77b57a945f19
Rayhan Gusty Alif <rayhangustyalif@gmail.com>
2020-01-28 16:06:41 +0700

    Fix my fucc

--- app/models.py
+++ app/models.py
@@ -8,6 +8,8 @@ class Akun(db.Model):
     ref = db.Column(db.String(3), nullable=False, primary_key=True)
     nama = db.Column(db.String(30), nullable=False)
 
+    details = db.relationship('Jurnal_Detail', backref='jurnal', lazy='dynamic')
+
     def __repr__(self):
         return "<Akun {} - {}>".format(self.ref, self.nama)
 
@@ -18,7 +20,7 @@ class Jurnal(db.Model):
     uraian = db.Column(db.String(50), nullable=False)
     tanggal = db.Column(db.DateTime, nullable=False, default=datetime.utcnow())
 
-    details = db.relationship('Jurnal_Detail', backref='jurnal', lazy='dynamic')
+    # details = db.relationship('Jurnal_Detail', backref='jurnal', lazy='dynamic')
 
     def __repr__(self):
         return "<Jurnal {} - {}>".format(self.uraian, self.tanggal)
@@ -33,5 +35,32 @@ class Jurnal_Detail(db.Model):
     nominal = db.Column(db.Float, nullable=False)
     dk = db.Column(db.String(1), nullable=False)
 
+    dp = db.relationship('Jurnal_Detail_Penjualan', backref='jurnal_detail', lazy='dynamic')
+
     def __repr__(self):
         return "<Jurnal_Detail {} - {} - {} - {}>".format(self.jurnal_id, self.akun_ref, self.nominal, self.dk)
+
+class Barang(db.Model):
+    __tablename__ = 'barang'
+
+    id = db.Column(db.Integer, primary_key=True)
+    nama = db.Column(db.String(30), nullable=False)
+    harga_pokok = db.Column(db.Integer, nullable=False)
+    harga_jual = db.Column(db.Integer, nullable=False)
+    stok = db.Column(db.Integer, nullable=False)
+
+    dp = db.relationship('Jurnal_Detail_Penjualan', backref='barang', lazy='dynamic')
+
+    def __repr__(self):
+        return "<Jurnal_Detail {} - {} - {} - {} - {}>".format(self.id, self.nama, self.harga_pokok, self.harga_jual, self.stok)
+
+
+class Jurnal_Detail_Penjualan(db.Model):
+    __tablename__ = 'jurnal_detail_penjualan'
+
+    jurnal_details_id = db.Column(db.Integer, db.ForeignKey('jurnal_detail.id'), primary_key=True)
+    barang_id = db.Column(db.Integer, db.ForeignKey('barang.id'), nullable=False)
+    nominal = db.Column(db.Float, nullable=False)
+
+    def __repr__(self):
+        return "<Jurnal_Detail_Penjualan {} - {} - {}>".format(self.jurnal_details_id, self.barang_id, self.nominal)
--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@ from flask import jsonify, make_response, request, abort, redirect, url_for
 import datetime
 
 from app import app, auth, db
-from app.models import Akun, Jurnal, Jurnal_Detail
+from app.models import Akun, Jurnal, Jurnal_Detail, Jurnal_Detail_Penjualan, Barang
 
 @app.route('/api/v1.0/akun/', methods=['GET', 'POST'])
 @auth.login_required
@@ -14,10 +14,16 @@ def akun_list():
         for akun in query:
             query_detail = Jurnal_Detail.query.filter_by(akun_ref=akun.ref).all()
             jurnal_details = []
+            saldo = 0
 
             for data in query_detail:
                 jurnal = Jurnal.query.filter_by(id=data.jurnal_id).first_or_404()
                 # print(jurnal)
+                if(data.dk == 'D'):
+                    saldo = saldo + data.nominal
+                else:
+                    saldo = saldo - data.nominal
+                    
                 jurnal_details.append({
                     'tanggal': jurnal.tanggal.strftime("%Y-%m-%dT%H:%M:%S"),
                     'uraian': jurnal.uraian,
@@ -28,7 +34,8 @@ def akun_list():
             res_list.append({
                 'ref': akun.ref,
                 'nama': akun.nama,
-                'details': jurnal_details
+                'details': jurnal_details,
+                'saldo': saldo
             })
 
         return jsonify({
@@ -58,6 +65,184 @@ def akun_list():
 
         return redirect(url_for('akun_list'))
         
+    return abort(405)
+
+@app.route('/api/v1.0/jurnal/', methods=['GET', 'POST'])
+@auth.login_required
+def jurnal_list():    
+    if request.method == 'GET':
+        query = Jurnal.query.all()
+        res_list = []
+
+        for jurnal in query:
+            query_detail = Jurnal_Detail.query.filter_by(jurnal_id=jurnal.id).all()
+            jurnal_details = []
+
+            for data in query_detail:
+                query_akun = Akun.query.filter_by(ref=data.akun_ref).first()
+                query_dp = Jurnal_Detail_Penjualan.query.filter_by(jurnal_details_id=data.id).first()
+
+                dp = None
+
+                if(query_dp):
+                    dp = {
+                        'barang_id': query_dp.barang_id,
+                        'nominal': query_dp.nominal
+                    }
+
+                jurnal_details.append({
+                    'id': data.id,
+                    'akun': query_akun.nama,
+                    'nominal': data.nominal,
+                    'dk': data.dk,
+                    'dp': dp
+                })
+
+            res_list.append({
+                'id': jurnal.id,
+                'uraian': jurnal.uraian,
+                'tanggal': jurnal.tanggal.strftime("%d/%m/%Y %H:%M:%S"),
+                'details': jurnal_details
+            })
+        return jsonify({
+            'jurnal_list': res_list
+        })
+
+    if request.method == 'POST':
+        ### Restrict to JSON only ###
+        if not request.is_json:
+            return abort(400)
+
+        req_data = request.get_json()
+        
+        data_details = req_data['details']
+
+        if not isinstance(req_data, dict) and not isinstance(data_details, list):
+            return abort(400)
+
+        jurnal_baru = Jurnal(
+            uraian=req_data['uraian'],
+            tanggal=datetime.datetime.strptime(req_data['tanggal'], "%d/%m/%Y %H:%M:%S") 
+        )
+
+        db.session.add(jurnal_baru)
+        db.session.commit()
+        
+        for data in data_details:
+            detail_baru = Jurnal_Detail(
+                jurnal_id=jurnal_baru.id,
+                akun_ref=data['akun_ref'],
+                nominal=float(data['nominal']),
+                dk=data['dk'].upper()
+            )
+
+            db.session.add(detail_baru)
+            db.session.commit()
+
+            try:
+                dp = data['dp']
+                dp_baru = Jurnal_Detail_Penjualan(
+                    jurnal_details_id=detail_baru.id,
+                    barang_id=dp['barang_id'],
+                    nominal=dp['nominal']
+                )
+                db.session.add(dp_baru)
+                db.session.commit()
+            except KeyError:
+                pass
+
+            # print(detail_baru)
+
+        ##### Check if data already exist ####
+        # if Jurnal.query.filter_by(uraian=req_data['uraian']).first():
+        #     return abort(409)
+
+        return redirect(url_for('jurnal_list'))
+
+    return abort(405)
+
+@app.route('/api/v1.0/barang/', methods=['GET', 'POST'])
+@auth.login_required
+def barang_list():
+    if request.method == 'GET':
+        query = Barang.query.all()
+        res_list = []
+
+        for barang in query:
+            res_list.append({
+                'id': barang.id,
+                'nama': barang.nama,
+                'harga_pokok': barang.harga_pokok,
+                'harga_jual': barang.harga_jual,
+                'stok': barang.stok
+            })
+
+        return jsonify({
+            'barang_list': res_list
+        })
+
+    if request.method == 'POST':
+        if not request.is_json:
+            return abort(400)
+
+        req_data = request.get_json()
+        
+        if not isinstance(req_data, dict):
+            return abort(400)
+
+        barang_baru = Barang(
+            nama=req_data['nama'],
+            harga_pokok=req_data['harga_pokok'],
+            harga_jual=req_data['harga_jual'],
+            stok=req_data['stok']
+        )
+
+        db.session.add(barang_baru)
+        db.session.commit()
+
+        return redirect(url_for('barang_list'))
+
+    return abort(405)
+
+@app.route('/api/v1.0/barang/<int:id>/', methods=['GET', 'PUT'])
+@auth.login_required
+def barang_by_id(id):
+    if request.method == 'GET':
+        query = Barang.query.filter_by(id=id).first_or_404()
+
+        return jsonify({
+            'barang': query
+        })
+
+    if request.method == 'PUT':
+        if not request.is_json:
+            return abort(400)
+
+        req_data = request.get_json()
+        
+        if not isinstance(req_data, dict):
+            return abort(400)
+
+        query = Barang.query.filter_by(id=id).first_or_404()
+
+        query.stok = query.stok + int(req_data['stok'])
+        
+        query = Barang.query.all()
+        res_list = []
+
+        for barang in query:
+            res_list.append({
+                'id': barang.id,
+                'nama': barang.nama,
+                'harga_pokok': barang.harga_pokok,
+                'harga_jual': barang.harga_jual,
+                'stok': barang.stok
+            })
+
+        return jsonify({
+            'barang_list': res_list
+        })
+
     return abort(405)
 
 @app.route('/api/v1.0/akun/<ref>/', methods=['GET', 'DELETE', 'PUT'])
@@ -139,78 +324,6 @@ def akun_by_ref(ref):
         return jsonify({
             "akun_list": res_list
         })
-
-    return abort(405)
-
-@app.route('/api/v1.0/jurnal/', methods=['GET', 'POST'])
-@auth.login_required
-def jurnal_list():    
-    if request.method == 'GET':
-        query = Jurnal.query.all()
-        res_list = []
-
-        for jurnal in query:
-            query_detail = Jurnal_Detail.query.filter_by(jurnal_id=jurnal.id).all()
-            jurnal_details = []
-
-            for data in query_detail:
-                query_akun = Akun.query.filter_by(ref=data.akun_ref).first()
-                jurnal_details.append({
-                    'id': data.id,
-                    'akun': query_akun.nama,
-                    'nominal': data.nominal,
-                    'dk': data.dk
-                })
-
-            res_list.append({
-                'id': jurnal.id,
-                'uraian': jurnal.uraian,
-                'tanggal': jurnal.tanggal.strftime("%d/%m/%Y %H:%M:%S"),
-                'details': jurnal_details
-            })
-        return jsonify({
-            'jurnal_list': res_list
-        })
-
-    if request.method == 'POST':
-        ### Restrict to JSON only ###
-        if not request.is_json:
-            return abort(400)
-
-        req_data = request.get_json()
-        
-        data_details = req_data['details']
-
-        if not isinstance(req_data, dict) and not isinstance(data_details, list):
-            return abort(400)
-
-        jurnal_baru = Jurnal(
-            uraian=req_data['uraian'],
-            tanggal=datetime.datetime.strptime(req_data['tanggal'], "%d/%m/%Y %H:%M:%S") 
-        )
-
-        db.session.add(jurnal_baru)
-        db.session.commit()
-        
-        for data in data_details:
-            detail_baru = Jurnal_Detail(
-                jurnal_id=jurnal_baru.id,
-                akun_ref=data['akun_ref'],
-                nominal=float(data['nominal']),
-                dk=data['dk'].upper()
-            )
-
-            print(detail_baru)
-
-            db.session.add(detail_baru)
-
-        db.session.commit()
-
-        ##### Check if data already exist ####
-        # if Jurnal.query.filter_by(uraian=req_data['uraian']).first():
-        #     return abort(409)
-
-        return redirect(url_for('jurnal_list'))
 
     return abort(405)
 
